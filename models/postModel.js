0bf7d85936777817beb6709bfe63facd923d1a7d
Inquisitive1124 <148849094+Inquisitive1124@users.noreply.github.com>
2025-02-07 23:17:50 +0530

    Update postModel.js

--- models/postModel.js
+++ models/postModel.js
@@ -5,6 +5,7 @@ const postSchema = new mongoose.Schema({
     post:{
         type:mongoose.Schema.Types.ObjectId,
         ref:"User",
+        required:true,
     },
     image:{
         type:String,

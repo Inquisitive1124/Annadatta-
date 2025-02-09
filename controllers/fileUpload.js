de57d273fc4d6bff920d05838b3004d38667d246
Inquisitive1124 <148849094+Inquisitive1124@users.noreply.github.com>
2025-02-09 16:39:48 +0530

    Update fileUpload.js
    Testing the post upload route

--- controllers/fileUpload.js
+++ controllers/fileUpload.js
@@ -1,98 +1,7 @@
 const File = require("../models/postModel");
 const cloudinary = require("cloudinary").v2
+const mongoose = require("mongoose")
 
-// exports.localFileUpload = async (req, res) => {
-//     try {
-
-//         const file = req.files.file; 
-//         console.log("FILE AAGYI JEE -> ",file);
-
-
-//         let path = __dirname + "/files/" + Date.now() + `.${file.name.split('.')[1]}`;
-//         console.log("PATH-> ", path)
-
-        
-//         file.mv(path , (err) => {
-//             console.log(err);
-//         });
-
-//         res.json({
-//             success:true,
-//             message:'Local File Uploaded Successfully',
-//         });
-
-//     }
-//     catch(error) {
-//         console.log("Not able to upload the file on server")
-//         console.log(error);
-//     }
-// }
-
-function isFileTypeSupported(type, supportedTypes) {
-    return supportedTypes.includes(type);
-}
-
-async function uploadFileToCloudinary(file, folder, quality) {
-    const options = {folder};
-    console.log("temp file path", file.tempFilePath);
-
-    if(quality) {
-        options.quality = quality;
-    }
-
-    options.resource_type = "auto";
-    return await cloudinary.uploader.upload(file.tempFilePath, options);
-}
-
-
-// exports.imageUpload = async (req, res) => {
-//     try{
-        
-//         const { description, address} = req.body;
-//         console.log(post,description, address);
-//         const post = req.user.id;
-//         console.log(post);
-
-//         const file = req.files.imageFile;
-//         console.log(file);
-
-//         const supportedTypes = ["jpg", "jpeg", "png"];
-//         const fileType = file.name.split('.')[1].toLowerCase();
-//         console.log("File Type:", fileType);
-
-//         if(!isFileTypeSupported(fileType, supportedTypes)) {
-//             return res.status(400).json({
-//                 success:false,
-//                 message:'File format not supported',
-//             })
-//         }
-
-//         console.log("Uploading to Cloudinary");
-//         const response = await uploadFileToCloudinary(file, "Food_application");
-//         console.log(response);
-
-//         const fileData = await File.create({
-//             post,
-//             description,
-//             address,
-//             image:response.secure_url,
-//         });
-
-//         res.json({
-//             success:true,
-//             imageUrl:response.secure_url,
-//             message:'Image Successfully Uploaded',
-//         })
-//     }
-//     catch(error) {
-//         console.error(error);
-//         res.status(400).json({
-//             success:false,
-//             message:'Something went wrong',
-//         });
-
-//     }
-// }
 function isFileTypeSupported(type, supportedTypes) {
     return supportedTypes.includes(type);
 }
@@ -159,4 +68,3 @@ exports.imageUpload = async (req, res) => {
         });
     }
 };
-
